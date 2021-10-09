e255a4b2f8a61cec6ae0af502f5cb55d6824601a
PClasson <classon.philip@gmail.com>
2021-10-09 14:44:47 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
+  assetPrefix: isProd ? "https://pclasson.github.io/resuMe" : "",
   reactStrictMode: true,
   images: {
     loader: "imgix",
