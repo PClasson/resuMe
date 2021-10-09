93795e9f61224b45b7220ab629931de941680337
PClasson <classon.philip@gmail.com>
2021-10-09 23:42:00 +0200

    Fonts

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,13 @@ import "tailwindcss/tailwind.css";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div className="min-h-screen bg-primary py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="absolute inset-0 bg-gray-100 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <div className="max-w-md mx-auto">
-            <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <p className="font-sans text-5xl font-thin text-center">
-                  UNDER CONSTRUCTION
-                </p>
-              </div>
-            </div>
-          </div>
+          <p className="font-SF-Compact text-5xl font-thin text-center">
+            UNDER CONSTRUCTION
+          </p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#1f2147",
+      secondary: "#574373",
+    }),
+    gradientColorStops: (theme) => ({
+      ...theme("colors"),
+      primary: "#1f2147",
+      secondary: "#702429",
+    }),
+    extend: {
+      fontFamily: {
+        sans: ["SF-Compact", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
